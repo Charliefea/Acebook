e66d2e27a547870b71c85dd171653889245e9555
Guillermo <geor@Guillermos-MacBook-Pro.local>
2017-06-27 12:34:16 +0100

    new user can see welcome page receiving him

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    session[:current_user_id] = @user.id
     redirect_to users_url
+  end
+
+  def index
+    @user = User.find_by_id(session[:current_user_id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "POST /" do
+    it "responds with 200 " do
+      post :create, params: { user: { username: "Guillermo" } }
+      expect(response).to redirect_to(users_url)
+    end
+
+    it "creates a new user" do
+      post :create, params: { user: { username: "geor" } }
+      expect(User.find_by(username: "geor")).to be
+    end
+  end
+
 end
